6711ab30fab2c77867b95bbb9293e4545e2746e9
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-01-25 17:00:28 +0000

    implement redirect for post /urls

--- express_server.js
+++ express_server.js
@@ -1,26 +1,61 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
 //set ejs as a template engine//
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  sms5xK: "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${shortURL}`);
+  console.log(`302 Found`);
+});
+
+app.get("/urls/new", (req, res) => {
+  console.log(`200 Ok`);
+  res.render("urls_new");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
+  console.log(`200 Ok`);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
-app.get("/hello", (req, res) => [
-  res.send("<html><body>Hello <b>World</b></body></html>\n"),
-]);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port${PORT}`);
